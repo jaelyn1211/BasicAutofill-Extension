085bc301cfe2b3c2f28a1316c0707ab4d3664f83
Jaelyn <jaelyn1211@berkeley.edu>
2015-05-15 19:06:06 -0700

    First Commit

---
+++ js/content.js
@@ -0,0 +1,31 @@
+// Javascript File
+
+
+var username;
+var userEmail;
+var email;
+var Email;
+var pass;
+var Passwd;
+var password;
+var userPassword;
+
+
+
+$(document).ready(function(){
+	
+	//code
+	if ($('.error').length == 0) {
+		$('input#username').val('my name')
+		$('input#login-username').val('myemail@email.com')
+		$('input#login-passwd').val('password')
+		$('input#userEmail').val('myemail@email.com')
+		$('input#userPassword').val('password')
+		$('input#email').val('myemail@email.com')
+		$('input#Email').val('myemail@email.com')
+		$('input#pass').val('password')
+		$('input#Passwd').val('password')
+		$('input#password').val('password')
+		;
+	}
+});
